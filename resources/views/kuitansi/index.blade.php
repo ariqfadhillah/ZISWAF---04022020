628a96a60b64955ee077ec385ad51ac1cf831ea1
ariqfadhillah <ariqsaja99@gmail.com>
2020-03-09 20:20:52 +0700

    addding ajax jquery

--- resources/views/kuitansi/index.blade.php
+++ resources/views/kuitansi/index.blade.php
@@ -156,9 +156,9 @@
 								@endif
 							</div>
 
-							<div class="form-group{{$errors->has('nama_muzakki') ? ' has-error' : ''}}">
+							<div id="muzakki" class="form-group{{$errors->has('nama_muzakki') ? ' has-error' : ''}}">
 								<label for="exampleInputEmail1">Atas Nama (Daftar Nama Muzakki)</label>
-								<input name="nama_muzakki" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan Nama Muzakki" value="{{old('nama_muzakki')}}">
+								<input name="nama_muzakki" type="text" class="form-control" id="exampleInputnama_muzakki" aria-describedby="emailHelp" placeholder="Masukan Nama Muzakki" value="{{old('nama_muzakki')}}"> <br>
 								@if($errors->has('nama_muzakki'))
 								<span class="help-block">{{$errors->first('nama_muzakki')}}</span>
 								@endif
@@ -202,5 +202,25 @@
             ]
            });
 	} );
+</script>
+<script>
+	$(document).ready(function() {
+	var max_fields      = 10; //maximum input boxes allowed
+	var wrapper   		= $("#muzakki"); //Fields wrapper
+	var add_button      = $("#exampleInputnama_muzakki"); //Add button ID
+	
+	var x = 1; //initlal text box count
+	$(add_button).click(function(e){ //on add input button click
+		e.preventDefault();
+		if(x < max_fields){ //max input box allowed
+			x++; //text box increment
+			$(wrapper).append('<div><input name="nama_muzakki" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukan Nama Muzakki" value="{{old('nama_muzakki')}}"><a href="#" class="remove_field">Remove</a></div>'); //add input box
+		}
+	});
+	
+	$(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+		e.preventDefault(); $(this).parent('div').remove(); x--;
+	})
+});
 </script>
 @stop
